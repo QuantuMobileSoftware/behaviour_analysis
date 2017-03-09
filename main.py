268ba0f7c5dce65eb1c3f1014c6b5ec390480b91
exotikh3 <dotavsegor@gmail.com>
2017-03-09 19:00:53 +0200

    stable tree classifier

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import pandas as pd
+from sklearn import tree
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 import numpy as np
-import datetime as dt
+
+
+def is_not_null(x):
+    if pd.isnull(x):
+        return 0
+    else:
+        return 1
+
 
 # read dataset
-ds_training = pd.read_csv('outsource_training.csv')
+ds_training = pd.read_csv('outsource_training.csv', low_memory=False)
+
+# drop duplicates
+ds_training = ds_training.drop_duplicates()
 
 # features selection
 unrelevant_features = ['date', 'mezip', 'email_length', 'email_alpha_chars', 'email_numeric_chars',
@@ -62,12 +75,30 @@ ds_training[number_features] = ds_training[number_features].fillna(0)
 ds_training[text_features] = ds_training[text_features].fillna('miss')
 
 # processing data
-ds_training[date_features] = pd.to_datetime(ds_training[date_features].stack()).unstack()
+ds_training['created_at'] = pd.to_datetime(ds_training['created_at'])
 ds_training['diff'] = pd.to_datetime('today') - ds_training['created_at']
 ds_training.drop(['created_at'], inplace=True, axis=1)
+ds_training['diff'] = ds_training['diff'].astype('timedelta64[D]')
 
 # make target label
+ds_training['ccreate'] = ds_training['ccreate'].apply(is_not_null)
+target_data = ds_training['ccreate']
+ds_training.drop(['ccreate'], inplace=True, axis=1)
 
+# mapping
+ds_training = pd.get_dummies(ds_training)
 
-print ds_training.dtypes
-print ds_training.head
+#
+# using ML algorithms
+#
+# splitting dataframe
+x_train, x_test, y_train, y_test = train_test_split(ds_training, target_data, test_size=0.2, random_state=42)
+
+# using tree classifier
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(x_train, y_train)
+
+# test our model
+y_predicted = clf.predict(x_test)
+
+print accuracy_score(y_test, y_predicted)
